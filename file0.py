d7f0673cb53a4884c82fb175732b99977c96af67
Aluminium51 <noptanadon.p@gmail.com>
2025-01-06 23:04:32 +0700

    random and calculator

--- file0.py
+++ file0.py
@@ -1,2 +1,18 @@
 print("hello world222")
 print("Thailand")
+
+#calculator program
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+operation = input("Enter an operation: ")
+if operation == "+":
+    print(a + b)
+elif operation == "-":
+    print(a - b)
+elif operation == "*":
+    print(a * b)
+elif operation == "/":
+    print(a / b)
+else:
+    print("Invalid operation")
+    
--- file1.py
+++ file1.py
@@ -1,2 +1,10 @@
-print("2")
-print("America")
+import random
+
+cards = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+deck = []
+for card in cards:
+    for suit in suits:
+        deck.append(f'{card} of {suit}')
+random.shuffle(deck)
+print(deck.pop())
